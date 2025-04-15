12114225e8b640d1d2e901954fd548bb3160e84a
GitHub Action <action@github.com>
2025-04-15 20:45:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/maciekwiso/pyfunrun/services/MyProjectService.kt
+++ src/main/kotlin/com/github/maciekwiso/pyfunrun/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.maciekwiso.pyfunrun.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.maciekwiso.pyfunrun.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
