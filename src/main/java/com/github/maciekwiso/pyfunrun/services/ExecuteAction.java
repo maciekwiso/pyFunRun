dae84a971e35f19453698a67c150ae07777d25e3
maciek <maciekwiso@o2.pl>
2025-04-16 18:39:27 +0200

    init version of the plugin

---
+++ src/main/java/com/github/maciekwiso/pyfunrun/services/ExecuteAction.java
@@ -0,0 +1,41 @@
+package com.github.maciekwiso.pyfunrun.services;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.jetbrains.python.psi.PyFunction;
+import org.jetbrains.annotations.NotNull;
+
+import static com.github.maciekwiso.pyfunrun.services.FunExecutor.execute;
+
+public class ExecuteAction extends AnAction {
+    private static Logger log = Logger.getInstance(ExecuteAction.class);
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        log.debug("ExecuteAction fired");
+        var pyFun = pyFunAtCaret(e);
+        if (pyFun != null) {
+            execute(pyFun);
+        }
+    }
+
+    private PyFunction pyFunAtCaret(AnActionEvent anActionEvent) {
+        Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
+        PsiFile psiFile = anActionEvent.getData(CommonDataKeys.PSI_FILE);
+        if (editor == null || psiFile == null) {
+            return null;
+        }
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement element = psiFile.findElementAt(offset);
+        if (element != null) {
+            return PsiTreeUtil.getParentOfType(element, PyFunction.class);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/github/maciekwiso/pyfunrun/services/FunExecutor.java
@@ -0,0 +1,52 @@
+package com.github.maciekwiso.pyfunrun.services;
+
+import com.intellij.execution.ProgramRunnerUtil;
+import com.intellij.execution.RunManager;
+import com.intellij.execution.RunnerAndConfigurationSettings;
+import com.intellij.execution.executors.DefaultRunExecutor;
+import com.intellij.openapi.diagnostic.Logger;
+import com.jetbrains.python.psi.PyFunction;
+import com.jetbrains.python.run.PythonConfigurationType;
+import com.jetbrains.python.run.PythonRunConfiguration;
+
+public class FunExecutor {
+
+    private static Logger log = Logger.getInstance(FunExecutor.class);
+
+    public static void execute(PyFunction pyFunction) {
+        if (isExecutable(pyFunction)) {
+            log.debug("Executing function " + pyFunction.getName());
+            var config = createSdkRunConfiguration(pyFunction);
+            add(config);
+            run(config);
+        }
+    }
+
+    public static boolean isExecutable(PyFunction pyFunction) {
+        return pyFunction != null && pyFunction.getName().startsWith("run");
+    }
+
+    private static RunnerAndConfigurationSettings createSdkRunConfiguration(PyFunction pyFunction) {
+        var runManager = RunManager.getInstance(pyFunction.getProject());
+        var configuration = runManager.createConfiguration("PyFunRun " + pyFunction.getName(), PythonConfigurationType.class);
+        var pythonRunConfiguration = (PythonRunConfiguration)configuration.getConfiguration();
+        pythonRunConfiguration.setName("pyFunRun " + pyFunction.getName());
+        pythonRunConfiguration.setUseModuleSdk(true);
+        pythonRunConfiguration.setScriptName(pyFunction.getContainingFile().getName());
+        pythonRunConfiguration.setModuleMode(false);
+        pythonRunConfiguration.setWorkingDirectory(pyFunction.getContainingFile().getContainingDirectory().getVirtualFile().getPath());
+        pythonRunConfiguration.setScriptParameters(pyFunction.getName());
+        return configuration;
+    }
+
+    private static void add(RunnerAndConfigurationSettings configuration) {
+        var runManager = RunManager.getInstance(configuration.getConfiguration().getProject());
+        runManager.addConfiguration(configuration);
+        runManager.setSelectedConfiguration(configuration);
+    }
+
+    private static void run(RunnerAndConfigurationSettings configuration) {
+        var executor = DefaultRunExecutor.getRunExecutorInstance();
+        ProgramRunnerUtil.executeConfiguration(configuration, executor);
+    }
+}
